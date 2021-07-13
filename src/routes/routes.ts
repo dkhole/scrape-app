7fd6cb078db22d0e3131650bcdb9c83b1e8a360d
dkhole <dkhlee419@gmail.com>
2021-07-13 22:42:07 +1000

    correctly serving of build folder

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,9 +6,8 @@ import { router as auth } from './auth';
 import { verifyAuth } from '../services/auth/verify';
 
 export const routes = (app: express.Application) => {
-    app.get('/', (req, res) => {
-        res.send('Welcome to port 3000');
-    });
+
+    app.use(express.static(path.join(__dirname, '..', '..', 'client/build')));
     
     app.use('/api', auth, scrape, autoMsg);
 
@@ -18,7 +17,7 @@ export const routes = (app: express.Application) => {
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+        res.sendFile(path.join(__dirname, '..', '..', 'client/build/index.html'));
     });
 
 
