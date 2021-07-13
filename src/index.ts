0e137cfdc8c4ce3c6a85a055e674f1122c56520b
dkhole <dkhlee419@gmail.com>
2021-07-13 22:06:08 +1000

    fixing port logic

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { load } from './loaders/load';
 import { routes } from './routes/routes';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app: express.Application = express();
 
 load(app);
 routes(app);
 
-let port = parseInt(process.env.PORT);
-if (port == null ) {  port = 8000; }
+const processPort: string = process.env.PORT ?? '3000';
+const port: number = parseInt(processPort);
 
 app.listen(port, () => {
     console.log(`The application is listening on port ${port}!`);
